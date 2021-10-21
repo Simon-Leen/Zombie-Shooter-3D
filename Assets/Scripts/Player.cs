3fadeca0a163d1a934240b6757783077e1bef184
= <=>
2021-10-21 06:29:02 +0100

    Finished Characrter

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private CharacterController _controller;
+    [Header("Controller Settings")]
     [SerializeField] private float _speed = 6.0f;
     [SerializeField] private float _jumpSpeed = 8.0f;
     [SerializeField] private float _gravity = 20.0f;
     private Vector3 _direction, _velocity;
+
+    private Camera _mainCam;
+    [Header("Camera Settings")]
+    [SerializeField] private float _camSensitivity = 2.0f;
 
     void Start()
     {
@@ -17,9 +22,28 @@ public class Player : MonoBehaviour
         {
             Debug.LogError("Character Controller is NULL");
         }
+
+        _mainCam = Camera.main;
+        if (_mainCam == null)
+        {
+            Debug.LogError("Main Camera is NULL");
+        }
+
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
+    {
+        Movement();
+        MovementView();
+
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+
+    void Movement()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
@@ -36,6 +60,21 @@ public class Player : MonoBehaviour
         }
 
         _velocity.y -= _gravity * Time.deltaTime;
+        _velocity = transform.TransformDirection(_velocity);
         _controller.Move(_velocity * Time.deltaTime);
+    }
+
+    void MovementView()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        Vector3 currentRot = transform.localEulerAngles;
+        currentRot.y += mouseX * _camSensitivity;
+        transform.localRotation = Quaternion.AngleAxis(currentRot.y, Vector3.up);
+
+        Vector3 currentCamRot = _mainCam.gameObject.transform.localEulerAngles;
+        currentCamRot.x -= mouseY * _camSensitivity;
+        _mainCam.gameObject.transform.localRotation = Quaternion.AngleAxis(currentCamRot.x, Vector3.right);
     }
 }
